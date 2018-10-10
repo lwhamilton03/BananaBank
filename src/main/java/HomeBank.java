244cebb0123897ab435496fda1e0664723d4fece
lwhamilton03 <lwhamilton03@outlook.com>
2018-10-10 12:28:48 +0100

    modify

--- src/main/java/HomeBank.java
+++ src/main/java/HomeBank.java
@@ -3,31 +3,58 @@ import java.awt.Button;
 import java.awt.Color;
 import java.awt.Frame;
 import java.awt.GridLayout;
+import java.awt.Label;
 import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class HomeBank {
+	Frame F; 
 	
 	public void createBankPage()
 	{
-		Frame F = new Frame("home"); 
+		F = new Frame("BANANA BANK"); 
 		F.setVisible(true);
 		F.setSize(600,600);
+	
+		Panel titleHeader = new Panel(); 
+		Label title = new Label("BANANA BANK"); 
+		titleHeader.setBackground(Color.PINK);
+		titleHeader.add(title);
+		F.add(titleHeader, BorderLayout.NORTH);
 		
 		Panel homePage = new Panel(); 
-		GridLayout grid = new GridLayout(3,1);
+		homePage.setVisible(true);
+		GridLayout grid = new GridLayout(4,1);
 		homePage.setLayout(grid);
 		F.add(homePage, BorderLayout.CENTER); 
 		
+	
+		
 		Button createAccount = new Button ("Create Account");
-		createAccount.setBackground(Color.BLUE);
+		createAccount.setBackground(Color.lightGray);
 		Button depositeMoney = new Button("Deposite Money");
-		depositeMoney.setBackground(Color.green);
+		depositeMoney.setBackground(Color.lightGray);
 		Button withdrawMoney = new Button("Withdraw"); 
-		withdrawMoney.setBackground(Color.PINK); 
+		withdrawMoney.setBackground(Color.lightGray); 
+		Button exit = new Button("EXIT"); 
+		exit.setBackground(Color.GRAY);
+		exit.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent eee)
+			{
+				F.setVisible(false);
+			}
+		});
+		
 		
 		homePage.add(createAccount);
 		homePage.add(depositeMoney);
 		homePage.add(withdrawMoney);
+		homePage.add(exit);
+		
+		
+		
 		
 		CreateAccount create = new CreateAccount(F);
 		createAccount.addActionListener(create);
